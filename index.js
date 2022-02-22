924c3e4c19f708ff1ab27e406db4dbbbe83cb68c
Adam Cox <73854883+AdamCox15@users.noreply.github.com>
2022-02-21 20:23:53 -0700

    Added some other comparators

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ prompt("What's thier name?");
 var loveScore = Math.random() * 100;
 loveScore = Math.floor(loveScore) + 1;
 
-if (loveScore > 80) {
+if (loveScore > 70) {
   alert("Your chance at love is " + loveScore + "%." + " You love each other like fish love water!");
-} else {
-  alert("Your chance at love is " + loveScore + "%.");
+}
+if (loveScore > 30 && loveScore <= 70) {
+     alert("Your chance at love is " + loveScore + "%.");
+  }
+
+if (loveScore <= 30) {
+  alert("Your chance at love is " + loveScore + "%." + " You go together like oil and water!");
 }
